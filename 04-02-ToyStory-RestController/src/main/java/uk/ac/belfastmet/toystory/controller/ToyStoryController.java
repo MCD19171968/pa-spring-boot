5bf0b5ca54c95a1688297c505f46b6ad49a1998c
MCD19171968 <55384934+MCD19171968@users.noreply.github.com>
2019-09-17 16:10:02 +0100

    webpage html
    html home

--- 04-02-ToyStory-RestController/src/main/java/uk/ac/belfastmet/toystory/controller/ToyStoryController.java
+++ 04-02-ToyStory-RestController/src/main/java/uk/ac/belfastmet/toystory/controller/ToyStoryController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class ToyStoryController {
 	
+	@GetMapping("/")
+	public String home() {
+		return "Toy Story Characters";
+	}
+	
 	@GetMapping("/woody")
 	public String woody() {
 		return "Woody was played by Tom Hanks";
@@ -19,14 +24,7 @@ public class ToyStoryController {
 		return "Buzz was played by Tim Allen";
 	}
 	
-	@GetMapping("/andy")
-	public String andy() {
-		return "Andy was played by ...";
-	}
 	
-	@GetMapping("/rex")
-	public String rex() {
-		return "Rex was played by ...";
-	}
+	
 
 }
