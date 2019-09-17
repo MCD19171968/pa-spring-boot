1576ba65bfecaa24118250bd81684c80cfa5cd2e
MCD19171968 <55384934+MCD19171968@users.noreply.github.com>
2019-09-17 15:06:44 +0100

    Toy Story
    @ rest controller

---
+++ 04-02-ToyStory-RestController/src/main/java/uk/ac/belfastmet/toystory/controller/HomeController.java
@@ -0,0 +1,38 @@
+package uk.ac.belfastmet.toystory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Home Page";
+	}
+	
+	@GetMapping("/Woody")
+	public String woody() {
+		return "Woody was played by Tom Hanks";
+	}
+	
+	@GetMapping("/Buzz")
+	public String buzz() {
+		return "Buzz was played by Tim Allen";
+	}
+	
+	@GetMapping("/Andy")
+	public String andy() {
+		return "Andy was played by ...";
+	}
+	
+	@GetMapping("/Rex")
+	public String rex() {
+		return "Rex was played by ...";
+	}
+	
+	
+
+}
