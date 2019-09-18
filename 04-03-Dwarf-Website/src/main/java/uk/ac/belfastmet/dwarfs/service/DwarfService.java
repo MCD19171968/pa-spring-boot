9f1ca87b2fcfa4194f26674c7c8098403f5cdc2c
MCD19171968 <55384934+MCD19171968@users.noreply.github.com>
2019-09-18 10:57:07 +0100

    html added
    dwarfs and html

---
+++ 04-03-Dwarf-Website/src/main/java/uk/ac/belfastmet/dwarfs/service/DwarfService.java
@@ -0,0 +1,33 @@
+package uk.ac.belfastmet.dwarfs.service;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import uk.ac.belfastmet.dwarfs.domain.Dwarf;
+
+@Service
+public class DwarfService {
+
+	private ArrayList<Dwarf> disneyDwarfs;
+	private ArrayList<Dwarf> tolkienDwarfs;
+
+	public ArrayList<Dwarf> getDisneyDwarfs(){
+
+		this.disneyDwarfs = new ArrayList<Dwarf>();
+
+		this.disneyDwarfs.add(new Dwarf("Sleepy", "Disney", "Sleepy.png"));
+		this.disneyDwarfs.add(new Dwarf("Happy", "Disney", "Happy.png"));
+		this.disneyDwarfs.add(new Dwarf("Bashful", "Disney", "Bashful.png"));
+		this.disneyDwarfs.add(new Dwarf("Dopey", "Disney", "Dopey.png"));
+		this.disneyDwarfs.add(new Dwarf("Doc", "Disney", "Doc.png"));
+		this.disneyDwarfs.add(new Dwarf("Grumpy", "Disney", "Grumpy.png"));
+		this.disneyDwarfs.add(new Dwarf("Sneezy", "Disney", "Sneezy.png"));
+
+		return this.disneyDwarfs;
+
+	}
+	public ArrayList<Dwarf> getTolkienDwarfs(){
+
+		this.tolkienDwarfs = new ArrayList<Dwarf>();
+		//add dwarfs here!
+		return tolkienDwarfs;
+	}
+}
